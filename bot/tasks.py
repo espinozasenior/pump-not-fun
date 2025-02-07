977ca358ff317fb45f1e93dde312fac8406e8b64
Luis J. Espinoza <luis.espinoza@qompa.io>
2025-02-06 21:48:42 -0600

    feat(tasks): add keep_db_online task to maintain database alive

--- bot/tasks.py
+++ bot/tasks.py
@@ -3,6 +3,8 @@ from logger.logger import logger
 from datetime import datetime, timedelta, UTC
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pyrogram import Client
+from sqlalchemy import select
+from database.database import SmartWallet, AsyncSessionFactory
 
 def register_tasks(app: Client, tasks_app: AsyncIOScheduler):
     async def on_startup():
@@ -12,9 +14,22 @@ def register_tasks(app: Client, tasks_app: AsyncIOScheduler):
         me = await app.get_me()
         logger.info(f"{identity} account @{me.username} is running!")
 
+    async def keep_db_online():
+        async with AsyncSessionFactory() as session:
+            query = select(SmartWallet).limit(1)
+            result = await session.execute(query)
+            result.scalars().first()
+
     # Only run startup tasks if actually starting up
     tasks_app.add_job(
         on_startup,
         'date',
         run_date=datetime.now() + timedelta(seconds=3)  # Allow warmup time
+    )
+
+    tasks_app.add_job(
+        keep_db_online,
+        'interval',
+        minutes=4,
+        next_run_time=datetime.now(UTC) + timedelta(seconds=3)
     )
